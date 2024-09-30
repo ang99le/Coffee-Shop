db010ccaa2cc0f48eb37a0bc3d997f7bc3766d77
malak <malak.d993@gmail.com>
2024-09-30 18:07:56 +0300

    adding frameworks

---
+++ coffeeShop/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar (){
+    return(
+        <div>
+            <ul>
+                <li>Store</li>
+                <li>Favorite</li>
+                <li>Blog</li>
+                <li>About</li>
+            </ul>
+        </div>
+    )
+} 
